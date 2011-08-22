4e5bea34fdb9ff21eeab4ce6790663140ed2f935
Victor (Zainan) Zhou <victor@hinrg.cs.jhu.edu>
2011-08-22 17:26:33 -0400

    first

---
+++ migrate-users-and-groups.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mkdir /root/move
+
+export UGIDLIMIT=500
+
+#Creat user file, filter out all users for id larger than UGID limit
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > /root/move/passwd.mig #migrate users larger than UGIDLMIT
+
+#Create group file, filter out all users for id larger than UGID limit
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > /root/move/group.mig
+
+#Create shadow file corresponding to user account
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - |egrep -f - /etc/shadow > /root/move/shadow.mig 
+
+#Create gshadow file corresponding to the group accounts
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/group | tee - |egrep -f - /etc/gshadow > /root/move/gshadow.mig
